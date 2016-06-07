ab76f43e40c664e8865abf593c7a8db9fdc79eb0
lukas <lukas.westermann@gmail.com>
2016-06-07 15:06:23 +0200

    initial "import" of materialize-css

--- lib/materializer.rb
+++ lib/materializer.rb
@@ -3,5 +3,10 @@ require 'materializer/version'
 
 module Materializer
   class Engine < ::Rails::Engine
+    initializer 'materializer.assets.precompile' do |app|
+      %w(stylesheets javascripts fonts).each do |sub|
+        app.config.assets.paths << root.join('vendor', 'assets', sub).to_s
+      end
+    end
   end
 end
